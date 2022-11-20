19ad428bac341913841aa6dfb84d6710664a0334
abhiraj2 <abhiraj.official15@gmail.com>
2022-11-21 00:44:05 +0530

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const { group } = require("console");
 const mongoClient = require("mongodb").MongoClient
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //const mongourl = "mongodb://localhost:27017"
 const mongourl = "mongodb+srv://mongoquiz:mongodb_quizapp_deerabani@deerabani-cluster.zxudpkj.mongodb.net/?retryWrites=true&w=majority"
